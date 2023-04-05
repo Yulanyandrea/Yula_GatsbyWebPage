c5615565cf33593181ec1dd02f1c46b16e651fba
Yulany Munevar <andra_852@hotmail.com>
2023-04-05 18:32:20 -0500

    principal page

---
+++ src/components/name/Name.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './style.css';
+
+const Name = () => {
+  return (
+    <main className="ContainerName">
+      <img src="./images/background.jpg" alt="" className="ContainerName__background" />
+      <h1 className="ContainerName__Yula">Yula</h1>
+    </main>
+  )
+}
+
+export default Name;
---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import Name from '../../components/name/Name';
+import './style.css';
+
+const Home = () => {
+  return (
+    <section className="container">
+       <Name />
+    </section>
+
+  )
+}
+export default Home
