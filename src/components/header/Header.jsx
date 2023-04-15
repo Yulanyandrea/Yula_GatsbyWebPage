3bc0c8c44e2134d55704409e1ce5b2433874aece
Yulany Munevar <andra_852@hotmail.com>
2023-04-15 09:09:32 -0500

    react router

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import note from '../../images/note.png';
 import music from '../../images/music.png';
 import Background from '../background/Background';
+// import { Link } from 'react-router-dom';
 import './style.css';
 
 const Header = () => {
@@ -10,6 +11,7 @@ const Header = () => {
       <Background />
       <img src={note} alt="" className="ContainerHeader__note" />
       <img src={music} alt="" className="ContainerHeader__note--two" />
+      {/* <Link to="#miSeccion"></Link> */}
       <button className="containerHeader__home">Home</button>
       <button className="containerHeader__home">About</button>
       <button className="containerHeader__home">software development</button>
