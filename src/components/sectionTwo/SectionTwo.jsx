f2cc4b7c6cc2afe072b703ec01b1cc1e53ce6df0
Yulany Munevar <andra_852@hotmail.com>
2023-04-15 14:19:56 -0500

    adding profile picture and icons to section two

--- src/components/sectionTwo/SectionTwo.jsx
+++ src/components/sectionTwo/SectionTwo.jsx
@@ -1,7 +1,13 @@
 import * as React from 'react';
+import yula from '../../images/yulaPhoto.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 import './style.css';
 
 const SectionTwo = () => {
+  // const handleLinkedin = () => {
+  //   window.open('https://www.linkedin.com/in/yulanymunevar/','_blank')
+  // }
   return (
     <main className="sectionTwo" id="sectionTwo">
       <h1 className="sectionTwo__about">About Me</h1>
@@ -12,6 +18,17 @@ const SectionTwo = () => {
           great applications to converge art and technology. Usually, I develop in React, Gatsby, Typescript
           Express, GraphQl, Mongo DB and Pure data.
         </p>
+        <img src={yula} alt="" className="sectionTwo__profilePicture" />
+        <section className="sectionTwo__socialMedia">
+          <a href="https://www.linkedin.com/in/yulanymunevar/" target="_blank" rel="noopener noreferrer" className="sectionTwo__socialMedia--link">
+            <FontAwesomeIcon icon={faLinkedin} className="sectionTwo__socialMedia--icon"/>
+          </a>
+
+          <a href="https://github.com/Yulanyandrea" target="_blank" rel="noopener noreferrer" className="sectionTwo__socialMedia--link">
+            <FontAwesomeIcon icon={faGithub} className="sectionTwo__socialMedia--icon"/>
+          </a>
+        </section>
+
       </section>
 
     </main>
