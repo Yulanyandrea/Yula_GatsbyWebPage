e72e8e708a32588aab797b2d483d46b24213f70f
Yulany Munevar <andra_852@hotmail.com>
2023-04-19 15:59:00 -0500

    Netlify setting for forms in section five adding domain

--- src/components/sectionFive/SectionFive.jsx
+++ src/components/sectionFive/SectionFive.jsx
@@ -9,7 +9,7 @@ const SectionFive = () => {
 
   return (
     <form className="sectionFive" id="ContactMe" method="POST" data-netlify="true">
-       <input type="hidden" name="subject" value="Sales inquiry from mysitename.netlify.app" />
+       <input type="hidden" name="subject" value="Sales inquiry from yulany-andrea.netlify.app" />
         <h1 className="sectionFive__title">Contact Me</h1>
         <p className="sectionFive__info">Please fill you information if you want to ask or say something</p>
         <section className="sectionFive__message">
@@ -20,7 +20,7 @@ const SectionFive = () => {
           <label htmlFor="myInput" className="sectionFive__message--label">Subject</label>
           <input type="text" className="sectionFive__fullname" />
           <label htmlFor="myInput" className="sectionFive__message--label">Your message</label>
-          <textarea type="text"  className="sectionFive__message--input" />
+          <textarea type="text"  name="message" className="sectionFive__message--input" />
           <button className="sectionFive__btn" onClick={handleSend}>Send</button>
 
         </section>
