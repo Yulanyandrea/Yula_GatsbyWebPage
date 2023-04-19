b8fe6275607e8d497f55b92df4921e5d2a5c0146
Yulany Munevar <andra_852@hotmail.com>
2023-04-19 15:51:48 -0500

    Netlify setting for forms in section five

--- src/components/sectionFive/SectionFive.jsx
+++ src/components/sectionFive/SectionFive.jsx
@@ -2,22 +2,28 @@ import * as React from 'react';
 import './style.css';
 
 const SectionFive = () => {
-  return (
-    <form className="sectionFive" id="ContactMe">
-      <h1 className="sectionFive__title">Contact Me</h1>
-      <p className="sectionFive__info">Please fill you information if you want to ask or say something</p>
-      <section className="sectionFive__message">
-        <label htmlFor="myInput" className="sectionFive__message--label">Full name</label>
-        <input type="text" className="sectionFive__fullname" />
-        <label htmlFor="myInput" className="sectionFive__message--label">Email</label>
-        <input type="email" className="sectionFive__fullname" />
-        <label htmlFor="myInput" className="sectionFive__message--label">Subject</label>
-        <input type="text" className="sectionFive__fullname" />
-        <label htmlFor="myInput" className="sectionFive__message--label">Your message</label>
-        <textarea type="text"  className="sectionFive__message--input" />
-        <button className="sectionFive__btn">Send</button>
 
-      </section>
+  const handleSend = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <form className="sectionFive" id="ContactMe" method="POST" data-netlify="true">
+       <input type="hidden" name="subject" value="Sales inquiry from mysitename.netlify.app" />
+        <h1 className="sectionFive__title">Contact Me</h1>
+        <p className="sectionFive__info">Please fill you information if you want to ask or say something</p>
+        <section className="sectionFive__message">
+          <label htmlFor="myInput" className="sectionFive__message--label">Full name</label>
+          <input type="text" className="sectionFive__fullname" />
+          <label htmlFor="myInput" className="sectionFive__message--label">Email</label>
+          <input type="email" className="sectionFive__fullname" />
+          <label htmlFor="myInput" className="sectionFive__message--label">Subject</label>
+          <input type="text" className="sectionFive__fullname" />
+          <label htmlFor="myInput" className="sectionFive__message--label">Your message</label>
+          <textarea type="text"  className="sectionFive__message--input" />
+          <button className="sectionFive__btn" onClick={handleSend}>Send</button>
+
+        </section>
 
     </form>
   )
