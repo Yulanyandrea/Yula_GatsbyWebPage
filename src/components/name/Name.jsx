e7bcb03ecd8628391534506dc0fc06ac4367cbd6
Yulany Munevar <andra_852@hotmail.com>
2023-04-19 15:40:49 -0500

    change background image format

--- src/components/name/Name.jsx
+++ src/components/name/Name.jsx
@@ -1,10 +1,11 @@
 import * as React from 'react';
+import background from '../../images/background.jpg';
 import './style.css';
 
 const Name = () => {
   return (
     <main className="ContainerName" id="name">
-      <img src="./images/background.jpg" alt="" className="ContainerName__background" />
+      <img src={background} alt="" className="ContainerName__background" />
       <section className="containerName___elements">
         <h1 className="ContainerName__Yula">Yula</h1>
 
