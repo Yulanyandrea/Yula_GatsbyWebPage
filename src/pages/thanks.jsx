8c690fd48c57582c3071b17cd812941cf06e574f
Yulany Munevar <andra_852@hotmail.com>
2023-04-21 13:37:14 -0500

    style for submit page

--- src/pages/thanks.jsx
+++ src/pages/thanks.jsx
@@ -1,12 +1,16 @@
 import * as React from 'react';
+import { Link } from "gatsby"
+import './style.css';
 
 
 const thanks = () => {
   return (
     <main className="thanksSection" id="thanks">
-      <h2 className="thanks Section">Thank you </h2>
+      <h2 className="thanks Section">Thank you 💜 </h2>
       <p className="thanksSection__paragraph">Your mail has been sent. I will be communicating with you
-      in a few days </p>
+     as soon as I can </p>
+      <img src="https://media.giphy.com/media/mS6uPUhffZEYSBBmzx/giphy.gif" width="200" alt=""></img>
+      <Link to="/">Go home</Link>.
     </main>
   )
 }
