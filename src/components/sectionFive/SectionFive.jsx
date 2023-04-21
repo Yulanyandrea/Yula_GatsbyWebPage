e4a8985cc3b7b1f7ccefc6ad6eab2fad65a5319a
Yulany Munevar <andra_852@hotmail.com>
2023-04-20 19:55:20 -0500

    change form atribute

--- src/components/sectionFive/SectionFive.jsx
+++ src/components/sectionFive/SectionFive.jsx
@@ -1,54 +1,29 @@
 import * as React from 'react';
-import { useState } from 'react';
-import { navigate } from 'gatsby';
 import './style.css';
 
 const SectionFive = () => {
-  const [showModal, setShowModal] = useState(false);
-
-  const [formData, setFormData] = useState({
-    fullName: "",
-    email: "",
-    subject: "",
-    message: ""
-  });
-
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    const myForm = event.target;
-    const formData = new FormData(myForm);
-
-    fetch("/", {
-      method: "POST",
-      headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: new URLSearchParams(formData).toString(),
-    })
-      .then(() => navigate("/thank-you/"))
-      .catch((error) => alert(error));
-  };
-
-
-  const handleInputChange = e => {
-    setFormData({ ...formData, [e.target.name]: e.target.value });
-  };
-
-
 
 
   return (
-    <form name="contact" className="sectionFive" id="ContactMe" method="POST" data-netlify="true" onSubmit={handleSubmit}>
+    <form name="contact"
+        className="sectionFive"
+        id="ContactMe"
+        method="POST"
+        data-netlify="true"
+        onSubmit="submit">
+          <input type="hidden" name="contact" value="contact" />
+
         <h1 className="sectionFive__title">Contact Me</h1>
         <p className="sectionFive__info">Please fill you information if you want to ask or say something</p>
         <section className="sectionFive__message">
           <label htmlFor="myInput" className="sectionFive__message--label" >Full name</label>
-          <input type="text" name="fullname" className="sectionFive__fullname"  onChange={handleInputChange} />
+          <input type="text" name="fullname" className="sectionFive__fullname"   />
           <label htmlFor="myInput" className="sectionFive__message--label">Email</label>
-          <input type="email" name="email" className="sectionFive__fullname" onChange={handleInputChange}/>
+          <input type="email" name="email" className="sectionFive__fullname" />
           <label htmlFor="myInput" className="sectionFive__message--label">Subject</label>
-          <input type="text" name="subject"  className="sectionFive__fullname" onChange={handleInputChange}/>
+          <input type="text" name="subject"  className="sectionFive__fullname" />
           <label htmlFor="myInput" className="sectionFive__message--label">Your message</label>
-          <textarea type="text"  name="message" className="sectionFive__message--input" onChange={handleInputChange}/>
+          <textarea type="text"  name="message" className="sectionFive__message--input" />
           <button type="submit" className="sectionFive__btn">Send</button>
 
         </section>
